9931ced9fc86c58a1914f193b44b0ea0d1aaf479
Strackeror <rremilavergne@gmail.com>
2022-06-29 18:46:08 +0200

    Improve spiribird multiplier hook method

--- ConvenientSpiribirds/convenientSpiribirds.lua
+++ ConvenientSpiribirds/convenientSpiribirds.lua
@@ -7,32 +7,29 @@ local SPIRIBIRD_MULTIPLIER = 3
 
 --- Plugin
 
+local modded_ids = {}
 sdk.hook(
-    sdk.find_type_definition("snow.gui.define.EquipDetailParamShortcut"):get_method("createLvBuffDataParam(snow.data.NormalLvBuffCageData)"),
-    function (args)
-    end,
-    function (retval)
-        local param = sdk.to_managed_object(retval)
-        local arr = param:get_field("statusBuffAddVal")
+    sdk.find_type_definition("snow.data.EquipmentInventoryData"):get_method("getLvBuffCageData"),
+    function(args) end,
+    function(retval)
+        local buffCageData = sdk.to_managed_object(retval)
+        local param = buffCageData:get_field("_Param")
+        local id = param:get_field("_Id")
+        if  modded_ids[id] then
+            return retval
+        end
+
+        
+        local arr = param:get_field("_StatusBuffAddValue")
+        
         for i, obj in ipairs(arr:get_elements()) do
             local value = obj:get_field("mValue")
-            local new_value = value * SPIRIBIRD_MULTIPLIER
-
-            arr[i - 1] = sdk.create_uint32(new_value)
+            
+            arr[i - 1] = sdk.create_uint32(value * SPIRIBIRD_MULTIPLIER)
         end
         
+        modded_ids[id] = true
         return retval
-    end
-)
-
-sdk.hook(
-    sdk.find_type_definition("snow.data.EquipDataManager"):get_method("getStatusBuffAddVal"), 
-    function(args) 
-        last_args = sdk.to_float(args[4])
-    end,
-    function(retval)
-        local value = sdk.to_int64(retval)
-        return sdk.to_ptr(value * SPIRIBIRD_MULTIPLIER)
     end
 )
 
